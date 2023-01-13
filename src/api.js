30117dab740cab1bcce32b931a314e30a61d3135
unfirthman <unfirthman@gmail.com>
2023-01-13 13:35:58 -0500

    search integration is complete

---
+++ src/api.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '86e0738ef2mshdb4c1d3be6dc19dp12e01ejsn9858946a8041',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+
+
