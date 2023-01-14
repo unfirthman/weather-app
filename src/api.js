dcff8526dd5e96b2b07cc9ecd7089a44b71ebe87
unfirthman <unfirthman@gmail.com>
2023-01-14 11:19:52 -0500

    initialized search module

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '86e0738ef2mshdb4c1d3be6dc19dp12e01ejsn9858946a8041',
+		'X-RapidAPI-Key': ' ',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
