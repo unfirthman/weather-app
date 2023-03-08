2157fafa13d6a678be5d8b0fb9d505981b69d084
unfirthman <unfirthman@gmail.com>
2023-03-08 14:23:16 -0500

    new CSS

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,6 +5,7 @@ function Header() {
    return (
    <div>
             <h1 className="header1">Wonderful Weather App</h1>
+            <h2 className='header2'>A simple weather app with lovely little icons</h2>
         </div>
    )
 }
